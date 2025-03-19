9db8a51c847834996d8bcdf4d6395798cc441d3e
Kacper Kubacki <kacper.kubacki@bitbag.pl>
2025-03-19 12:48:04 +0100

    DEC-5: Add buildForm method return type to fix build

--- src/Form/Type/ShippingGatewayType.php
+++ src/Form/Type/ShippingGatewayType.php
@@ -19,7 +19,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 final class ShippingGatewayType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('username', TextType::class, [
