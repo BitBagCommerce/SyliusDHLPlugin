0fb66c450d2b9aec1b1686b40b2870a32c334767
Kacper Kubacki <kacper.kubacki@bitbag.pl>
2025-03-19 11:57:04 +0100

    DEC-5: Add DHL shipping gateway configuration

---
+++ features/adding_shopping_gateway_configuration_dhl.feature
@@ -0,0 +1,26 @@
+@managing_shipping_gateway_dhl
+Feature: Creating shipping gateway
+  In order to export shipping data to external shipping provider service
+  As an Administrator
+  I want to be able to add new shipping gateway with shipping method
+
+  Background:
+    Given the store operates on a single channel in "United States"
+    And I am logged in as an administrator
+    And the store has "DHL Express" shipping method with "$6.44" fee
+
+  @ui
+  Scenario: Creating DHL Express shipping gateway
+    When I visit the create shipping gateway configuration page for "dhl"
+    And I select the "DHL Express" shipping method
+    And I fill the "Username" field with "123"
+    And I fill the "Password" field with "123"
+    And I fill the "Client id" field with "secret"
+    And I fill the "Client secret" field with "super_secret"
+    And I fill the "Name (first and last name or company name)" field with "Ja"
+    And I fill the "City" field with "Wawa"
+    And I fill the "Street" field with "Strasse"
+    And I fill the "House number" field with "1a"
+    And I fill the "Postal code" field with "12122"
+    And I add it
+    Then I should be notified that the shipping gateway has been created
---
+++ src/Form/Type/ShippingGatewayType.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusDhlPlugin\Form\Type;
+
+use BitBag\SyliusDhlPlugin\Enum\DhlEnvironmentEnum;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class ShippingGatewayType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'bitbag_sylius_dhl_plugin.ui.username',
+            ])
+            ->add('password', TextType::class, [
+                'label' => 'bitbag_sylius_dhl_plugin.ui.password',
+            ])
+            ->add('client_id', TextType::class, [
+                'label' => 'bitbag_sylius_dhl_plugin.ui.client_id',
+            ])
+            ->add('client_secret', TextType::class, [
+                'label' => 'bitbag_sylius_dhl_plugin.ui.client_secret',
+            ])
+            ->add('environment', ChoiceType::class, [
+                'choices' => [
+                    'bitbag_sylius_dhl_plugin.ui.environments.production' => DhlEnvironmentEnum::PRODUCTION,
+                    'bitbag_sylius_dhl_plugin.ui.environments.sandbox' => DhlEnvironmentEnum::SANDBOX,
+                ],
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'bitbag_sylius_dhl_plugin.ui.name',
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'bitbag_sylius_dhl_plugin.ui.city',
+            ])
+            ->add('street', TextType::class, [
+                'label' => 'bitbag_sylius_dhl_plugin.ui.street',
+            ])
+            ->add('house_number', TextType::class, [
+                'label' => 'bitbag_sylius_dhl_plugin.ui.house_number',
+            ])
+            ->add('postal_code', TextType::class, [
+                'label' => 'bitbag_sylius_dhl_plugin.ui.postal_code',
+            ]);
+    }
+}
